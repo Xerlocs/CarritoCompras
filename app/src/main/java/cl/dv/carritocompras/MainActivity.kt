d2d99c5f642e5bc231518faa7c5b6ac54c15ba93
Diego <Xerlocs@yahoo.com>
2023-11-22 18:10:17 -0300

    First Commit (toolbar)

--- app/src/main/java/cl/dv/carritocompras/MainActivity.kt
+++ app/src/main/java/cl/dv/carritocompras/MainActivity.kt
@@ -1,11 +1,36 @@
 package cl.dv.carritocompras
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_order -> {
+                //val intentOrder = Intent(this, SettingsActivity::class.java)
+                //startActivity(intentOrder)
+                return true
+            }
+            R.id.action_add -> {
+                //val intentAdd = Intent(this, AboutActivity::class.java)
+                //startActivity(intentAdd)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
