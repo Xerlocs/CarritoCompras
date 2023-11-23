f989d4f800c28eff8420fb97c65b6e95e2f827f8
Diego <Xerlocs@yahoo.com>
2023-11-23 10:04:01 -0300

    Second Commit (idiomas)

--- app/src/main/java/cl/dv/carritocompras/MainActivity.kt
+++ app/src/main/java/cl/dv/carritocompras/MainActivity.kt
@@ -5,15 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 private lateinit var cambio: TextView
+private lateinit var menu_orden: ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        menu_orden = findViewById(R.id.Menu_ordenar)
 
         cambio = findViewById(R.id.aaa)
     }
@@ -27,6 +32,7 @@ class MainActivity : AppCompatActivity() {
         when (item.itemId) {
             R.id.action_order -> {
                 cambio.setText(R.string.cambio)
+                menu_orden.visibility = View.VISIBLE
                 return true
             }
             R.id.action_add -> {
